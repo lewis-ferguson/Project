532eaa954053cd9863e57be67d5a2e0ac1099968
user <user@users-MacBook-Pro.local>
2023-03-22 16:03:05 +0000

    added a home page and more javascript to make the website more interactive

--- app.js
+++ app.js
@@ -1,4 +1,20 @@
 document.addEventListener('DOMContentLoaded', () => { 
-    const title = document.querySelector('h1');
-    title.textContent = 'Welcome to Bucket List Travels!';
+    // const title = document.querySelector('h1');
+    // title.textContent = 'Welcome to Bucket List Travels!';
+
+//     const form = document.querySelector('#form');
+//   form.addEventListener('submit', handleFormSubmit);
+
+
 });
+
+// //////////////////////////////////////////
+
+
+
+// const handleFormSubmit = function (event) {
+//     event.preventDefault();
+//     const resultAdventures = document.querySelector('#form-result');
+//     resultAdventures.textContent = `Your have selected: ${event.target.value}`
+//   };
+
